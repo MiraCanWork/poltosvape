e88c435ee21569681db20a1b64f3517d2864c71a
s1ned <s.1ned@yandex.ru>
2024-06-06 23:39:41 +0300

    fix: Header logo text color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className='flex justify-center items-center bg-white w-full md:py-6 py-[10px] px-5'>
       <div className='flex justify-between w-full max-w-[1340px]'>
-        <Logo textClassName='text-[#505050]' />
+        <Logo textClassName='!text-[#505050]' />
         <div className='flex items-center gap-[10px] text-[#505050] cursor-pointer'>
           <ProfileIcon />
           <span className='hidden md:inline md:leading-[29px] md:text-[24px] font-[300] text-center'>Вход</span>
